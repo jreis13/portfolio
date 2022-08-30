1cc5122f173a4a519cf9fa52375e962bbc1242cb
Joao Reis <joaofranciscoreis13@outlook.com>
2022-08-30 20:10:59 +0100

    wip work section

--- src/layout/Work/index.jsx
+++ src/layout/Work/index.jsx
@@ -1,9 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { AiFillEye, AiFillGithub } from "react-icons/ai";
+
+import { motion } from "framer-motion";
+
+import { AppWrap } from "../../wrapper";
 
 import "./index.scss";
 
 const Work = () => {
-  return <div>Work</div>;
+  const [activeFilter, setActiveFilter] = useState("All");
+  const [animateCard, setAnimateCard] = useState({ y: 0, opacity: 1 });
+
+  const categories = ["Web", "Mobile", "React", "All"];
+
+  const handleWorkFilter = (item) => {};
+
+  return (
+    <>
+      <h2 className="head-text">
+        Here are the <span>projects</span> I've contributed to.
+      </h2>
+
+      <div className="app__work-filter">
+        {categories.map((category, index) => (
+          <div
+            key={index}
+            onClick={() => handleWorkFilter(category)}
+            className={`app__work-filter-item app__flex p-text ${
+              activeFilter === category ? "item-active" : ""
+            }`}
+          >
+            {category}
+          </div>
+        ))}
+      </div>
+
+      <motion.div
+        animate={animateCard}
+        transition={{ duration: 0.5, delayChildren: 0.5 }}
+        className="app__work-portfolio"
+      ></motion.div>
+    </>
+  );
 };
 
 export default Work;
